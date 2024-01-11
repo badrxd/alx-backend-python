452aad96596e576d304b82d3c4339c2bb9e9ae74
badrxd <badrxdxd@gmail.com>
2024-01-11 15:54:03 +0100

    task 11. More involved type annotations

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Augment the following code with the correct duck-typed annotations:
+
+"""
+from typing import Dict, Optional, Any, TypeVar, Union
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Dict, key: Any, default:
+                     Union[T, None]) -> Union[Any, T]:
+    if key in dct:
+        return dct[key]
+    else:
+        return default
