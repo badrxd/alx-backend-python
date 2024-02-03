82a900157d395efd625e3653cfa79237e401c4e3
badrxd <badrxdxd@gmail.com>
2024-02-03 20:27:56 +0100

    fix the test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 '''unit test for utils methodes'''
+from parameterized import parameterized
 import unittest
 access_nested_map = __import__('utils').access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
 
-    def test_access_nested_map(self):
-        '''unit test for access_nested_map methodes'''
-        self.assertEqual(access_nested_map({"a": {"b": 2}}, ("a", "b")), 2)
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, result):
+        self.assertEqual(access_nested_map(nested_map, path), result)
 
 
 if __name__ == '__main__':
