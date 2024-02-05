19dbc1bf27f2bffa5e3ccdd989cbebf8831181c8
badrxd <badrxdxd@gmail.com>
2024-02-05 01:13:31 +0100

    task 7. Parameterize

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -47,6 +47,14 @@ class TestGithubOrgClient(unittest.TestCase):
             url.assert_called_once()
             payload.assert_called_once()
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, 'my_license', True),
+        ({"license": {"key": "other_license"}}, 'my_license', False)
+    ])
+    def test_has_license(self, repo, license_key, bl):
+        client = GithubOrgClient('google')
+        self.assertEqual(client.has_license(repo, license_key), bl)
+
 
 if __name__ == "__main__":
     unittest.main()
